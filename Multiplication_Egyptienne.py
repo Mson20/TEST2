d63cce0f9902596c31acc21f4ec88b3f22559612
Saamson <93591676+Mson20@users.noreply.github.com>
2022-03-01 16:03:59 +0100

    Add files via upload

---
+++ Multiplication_Egyptienne.py
@@ -0,0 +1,11 @@
+def multplication_egyptienne(A,B):
+    a=A
+    b=B
+    i=0
+    while a != 0:
+        if (a%2) == 1:
+            i=i+b
+        b=b*2
+        a=a//2
+    return(i)
+print(multplication_egyptienne(34,32))            
